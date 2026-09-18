4faf7be8f87c4ad10f5aa2a6968fd43b52cb07d5
Nathaniel Cleland <nathaniel.cleland@europlacer.co.uk>
2026-09-18 15:46:20 +0100

    Further cleanup

--- RawInput.Sharp/Native/RawInputMouseInfo.cs
+++ RawInput.Sharp/Native/RawInputMouseInfo.cs
@@ -1,36 +1,36 @@
-﻿using System.Runtime.InteropServices;
+﻿//using System.Runtime.InteropServices;
 
-namespace Linearstar.Windows.RawInput.Native;
+//namespace Linearstar.Windows.RawInput.Native;
 
-/// <summary>
-/// RID_DEVICE_INFO_MOUSE
-/// </summary>
-[StructLayout(LayoutKind.Sequential)]
-public readonly struct RawInputMouseInfo
-{
-    readonly int dwId;
-    readonly int dwNumberOfButtons;
-    readonly int dwSampleRate;
-    [MarshalAs(UnmanagedType.Bool)]
-    readonly bool fHasHorizontalWheel;
+///// <summary>
+///// RID_DEVICE_INFO_MOUSE
+///// </summary>
+//[StructLayout(LayoutKind.Sequential)]
+//public readonly struct RawInputMouseInfo
+//{
+//    readonly int dwId;
+//    readonly int dwNumberOfButtons;
+//    readonly int dwSampleRate;
+//    [MarshalAs(UnmanagedType.Bool)]
+//    readonly bool fHasHorizontalWheel;
         
-    /// <summary>
-    /// dwId
-    /// </summary>
-    public int Id => dwId;
+//    /// <summary>
+//    /// dwId
+//    /// </summary>
+//    public int Id => dwId;
 
-    /// <summary>
-    /// dwNumberOfButtons
-    /// </summary>
-    public int ButtonCount => dwNumberOfButtons;
+//    /// <summary>
+//    /// dwNumberOfButtons
+//    /// </summary>
+//    public int ButtonCount => dwNumberOfButtons;
 
-    /// <summary>
-    /// dwSampleRate
-    /// </summary>
-    public int SampleRate => dwSampleRate;
+//    /// <summary>
+//    /// dwSampleRate
+//    /// </summary>
+//    public int SampleRate => dwSampleRate;
 
-    /// <summary>
-    /// fHasHorizontalWheel
-    /// </summary>
-    public bool HasHorizontalWheel => fHasHorizontalWheel;
-}
+//    /// <summary>
+//    /// fHasHorizontalWheel
+//    /// </summary>
+//    public bool HasHorizontalWheel => fHasHorizontalWheel;
+//}
